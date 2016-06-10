62a1a3474299dc94b304e6b0c722df9cf23ba449
Erin Bell <rhodes@csu-137-148-120-204.csuohio.edu>
2016-06-10 13:13:22 -0400

    adds new req. parameters for tour location picker

--- custom_ui_fields/tours.php
+++ custom_ui_fields/tours.php
@@ -1,4 +1,4 @@
 <br><hr> 
 <p class="dashicons-before dashicons-location">&nbsp;<em>Drag locations from left to right to add to tour. Drag up and down to change order.</em></p>
 
-<?php history_tours_admin_object_picker($post,'tour_locations','tour_locations');?>
+<?php history_tours_admin_object_picker($post,'tour_locations','tour_locations','Locations','Tour');?>
